6211eac479ac68f7151e8f56f2070c27e36e3983
SByrne34 <102997923+SByrne34@users.noreply.github.com>
2023-03-04 00:02:01 +0000

    Update scene.js

--- scene.js
+++ scene.js
@@ -13,44 +13,19 @@ const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Define texture loader
-const textureLoader = new THREE.TextureLoader();
-
 // Create geometry
-const geometry = new THREE.SphereGeometry(1, 64, 64); // 1, 12, 12
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 
 // Define material
-const colorMap = textureLoader.load("earth.jpg");
-const material  = new THREE.MeshPhongMaterial({ map: colorMap });
+const material = new THREE.MeshNormalMaterial();
 
 // Create mesh
-const earth = new THREE.Mesh(geometry, material);
-scene.add(earth);
-
-// Define light
-const ambient = new THREE.AmbientLight(0x404040);
-scene.add(ambient);
-
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(0, 0, 1);
-scene.add(light);
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 // Move camera from center
 camera.position.x = 2;  // Move right from center of scene
 camera.position.y = 1;  // Move up from center of scene
-camera.position.z = 1;  // Move camera away from center of scene
+camera.position.z = 5;  // Move camera away from center of scene
 
-// Import camera control and rotation library
-const controls = new THREE.OrbitControls(camera, renderer.domElement); 
-controls.autoRotate = true;
-controls.autoRotateSpeed = 2;
-controls.noKeys = true;
-
-const render = function() {
-    requestAnimationFrame(render);
-
-    controls.update();
-    renderer.render(scene, camera);
-}
-
-render();
+renderer.render(scene, camera);
